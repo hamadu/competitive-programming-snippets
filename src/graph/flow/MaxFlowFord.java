fce1fc63c79bd1a79b6eaa2771ff2bd1fe14eeb4
hama_du <dercll1020@gmail.com>
2016-07-30 20:05:02 +0900

    flow algorithms

---
+++ src/graph/flow/MaxFlowFord.java
@@ -0,0 +1,67 @@
+package graph.flow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Maximum flow (ford-fulkerson).
+ */
+public class MaxFlowFord {
+    public class Edge {
+        int to;
+        int cap;
+        int rev;
+        public Edge(int _to, int _cap, int _rev) {
+            to = _to;
+            cap = _cap;
+            rev = _rev;
+        }
+    }
+
+    public List<Edge>[] graph;
+    public boolean[] used;
+
+    @SuppressWarnings("unchecked")
+    public void init(int size) {
+        graph = new List[size];
+        for (int i = 0; i < size ; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        used = new boolean[size];
+    }
+    public void addEdge(int from, int to, int cap) {
+        graph[from].add(new Edge(to, cap, graph[to].size()));
+        graph[to].add(new Edge(from, 0, graph[from].size() - 1));
+    }
+
+    private int dfs(int v, int t, int f) {
+        if (v == t) {
+            return f;
+        }
+        used[v] = true;
+        for (Edge e : graph[v]) {
+            if (!used[e.to] && e.cap > 0) {
+                int d = dfs(e.to, t, Math.min(f, e.cap));
+                if (d > 0) {
+                    e.cap -= d;
+                    graph[e.to].get(e.rev).cap += d;
+                    return d;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public int maxFlow(int s, int t) {
+        int flow = 0;
+        while (true) {
+            used = new boolean[graph.length];
+            int f = dfs(s, t, Integer.MAX_VALUE);
+            if (f == 0) {
+                break;
+            }
+            flow += f;
+        }
+        return flow;
+    }
+}
---
+++ test/data_structure/graph/flow/MaxFlowFordTest.java
@@ -0,0 +1,28 @@
+package data_structure.graph.flow;
+
+import graph.flow.MaxFlowFord;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxFlowFordTest {
+    @Test
+    public void init() {
+        MaxFlowFord maxFlow = new MaxFlowFord();
+        maxFlow.init(100);
+        assertThat(maxFlow.graph.length, is(100));
+        assertThat(maxFlow.maxFlow(0, 99), is(0));
+    }
+
+    @Test
+    public void addEdge() {
+        MaxFlowFord maxFlow = new MaxFlowFord();
+        maxFlow.init(10);
+
+        maxFlow.addEdge(0, 3, 100);
+        maxFlow.addEdge(3, 6, 50);
+        maxFlow.addEdge(6, 9, 70);
+        assertThat(maxFlow.maxFlow(0, 9), is(50));
+    }
+}
