aa7d9e5401bb5d2e0223f01f2ac3f11a77102b77
hama_du <dercll1020@gmail.com>
2016-08-22 20:50:00 +0900

    doced

--- src/data_structure/fenwick/FenwickTree.java
+++ src/data_structure/fenwick/FenwickTree.java
@@ -14,6 +14,14 @@ public class FenwickTree {
         data = new long[n+1];
     }
 
+    /**
+     * Computes value of [1, i].
+     *
+     * O(logn)
+     *
+     * @param i
+     * @return
+     */
     public long sum(int i) {
         long s = 0;
         while (i > 0) {
@@ -23,14 +31,39 @@ public class FenwickTree {
         return s;
     }
 
+    /**
+     * Computes value of [i, j].
+     *
+     * O(logn)
+     *
+     * @param i
+     * @param j
+     * @return
+     */
     public long range(int i, int j) {
         return sum(j) - sum(i-1);
     }
 
+    /**
+     * Sets value x into i-th position.
+     *
+     * O(logn)
+     *
+     * @param i
+     * @param x
+     */
     public void set(int i, long x) {
         add(i, x-range(i, i));
     }
 
+    /**
+     * Adds value x into i-th position.
+     *
+     * O(logn)
+     * 
+     * @param i
+     * @param x
+     */
     public void add(int i, long x) {
         while (i <= N) {
             data[i] += x;
