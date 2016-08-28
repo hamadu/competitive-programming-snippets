b605a93df0d1faa2f195388f4abc1e4563bf623e
hama_du <dercll1020@gmail.com>
2016-08-28 15:15:26 +0900

    euler-tour

---
+++ src/graph/tree/ParentCountOrder.java
@@ -0,0 +1,49 @@
+package graph.tree;
+
+/**
+ * Created by hama_du on 2016/08/26.
+ */
+public class ParentCountOrder {
+    /**
+     * Computes each node's {parent, number of vertices in its subtrees, BFS order} with given tree.
+     * It returns array of size 3*n.
+     *
+     * O(n)
+     *
+     * @param graph
+     * @param root
+     * @return
+     */
+    public static int[][] parentCountOrder(int[][] graph, int root) {
+        int n = graph.length;
+        int[] que = new int[2*n];
+        int[] parent = new int[n];
+        int[] bfsOrd = new int[n];
+        int[] cnt = new int[n];
+        int qh = 0, qt = 0;
+        que[qh++] = root;
+        que[qh++] = -1;
+        int vi = 0;
+        while (qt < qh) {
+            int now = que[qt++];
+            int par = que[qt++];
+            parent[now] = par;
+            bfsOrd[vi++] = now;
+            for (int to : graph[now]) {
+                if (to == par) {
+                    continue;
+                }
+                que[qh++] = to;
+                que[qh++] = now;
+            }
+        }
+        for (int i = n-1 ; i >= 0 ; i--) {
+            int now = bfsOrd[i];
+            cnt[now]++;
+            if (parent[now] != -1) {
+                cnt[parent[now]] += cnt[now];
+            }
+        }
+        return new int[][]{ parent, cnt, bfsOrd };
+    }
+}
---
+++ test/graph/tree/EulerTourTest.java
@@ -0,0 +1,56 @@
+package graph.tree;
+
+import org.junit.Test;
+
+import static graph.HeavyLightDecompositionTest.buildLineGraph;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by hama_du on 2016/08/27.
+ */
+public class EulerTourTest {
+    @Test
+    public void starGraph() {
+        int[][] tree = {
+                {1},
+                {0, 2, 3, 4},
+                {1},
+                {1},
+                {1}
+        };
+        assertThat(EulerTour.eulerTour(tree, 0), is(new int[]{0, 1, 4, 4, 3, 3, 2, 2, 1, 0}));
+    }
+
+    @Test
+    public void lineGraph() {
+        int[][] tree = buildLineGraph(11);
+        assertThat(EulerTour.eulerTour(tree, 0), is(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}));
+    }
+
+    @Test
+    public void nonObviousGraph() {
+        int[][] tree = {
+                {1, 2, 3},
+                    {0, 4, 5},
+                    {0, 9},
+                    {0, 11, 12, 13},
+                        {1, 6},
+                        {1, 7, 8},
+                            {4},
+                            {5},
+                            {5},
+
+                        {2, 10},
+                            {9},
+
+                        {3},
+                        {3},
+                        {3}
+        };
+        assertThat(EulerTour.eulerTour(tree, 0), is(new int[]{
+                0, 3, 13, 13, 12, 12, 11, 11, 3, 2, 9, 10, 10, 9, 2, 1, 5, 8, 8, 7, 7, 5, 4, 6, 6, 4, 1, 0
+        }));
+    }
+}
+
