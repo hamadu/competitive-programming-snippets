4c872c9fe281d27a07ed30b200a71aaf2f91c572
hama_du <dercll1020@gmail.com>
2016-10-24 21:45:52 +0900

    何故無かった

---
+++ src/graph/shortest_path/Dijkstra.java
@@ -0,0 +1,49 @@
+package graph.shortest_path;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Dijkstra {
+    int n;
+    int[][][] graph;
+
+    class State implements Comparable<State> {
+        int now;
+        long time;
+
+        State(int n, long t) {
+            now = n;
+            time = t;
+        }
+
+        @Override
+        public int compareTo(State o) {
+            return Long.compare(time, o.time);
+        }
+    }
+
+    public Dijkstra(int[][][] graph) {
+        this.n = graph.length;
+        this.graph = graph;
+    }
+
+    long[] doit(int from) {
+        long[] dp = new long[n];
+        Arrays.fill(dp, Long.MAX_VALUE / 10);
+        Queue<State> q = new PriorityQueue<>();
+        q.add(new State(from, 0));
+        dp[0] = 0;
+        while (q.size() >= 1) {
+            State st = q.poll();
+            for (int[] e : graph[st.now]) {
+                long time = st.time + e[1];
+                if (dp[e[0]] > time) {
+                    dp[e[0]] = time;
+                    q.add(new State(e[0], time));
+                }
+            }
+        }
+        return dp;
+    }
+}
